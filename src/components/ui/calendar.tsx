06b91bfcd321453ff62bab2601b9489a167fcf81
Sekou Samassa <SkSamassa@gmail.com>
2025-10-19 08:14:15 +0000

    Make the dropdown menu that allows to select the year/month scrollable w

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -26,7 +26,7 @@ function Calendar({
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
         caption_label: "hidden",
-        caption_dropdowns: "flex justify-center gap-1",
+        caption_dropdowns: "flex justify-center gap-2",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
