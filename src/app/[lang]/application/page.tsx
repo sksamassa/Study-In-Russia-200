6311aa1196ff796499a7a319b88ed905dcd3591a
Sekou Samassa <SkSamassa@gmail.com>
2025-10-20 14:54:03 +0000

    Server   Error: Route "/[lang]/application" used `params.lang`. `params`

--- src/app/[lang]/application/page.tsx
+++ src/app/[lang]/application/page.tsx
@@ -2,7 +2,8 @@ import MultiPageApplicationForm from "@/components/multi-page-application-form";
 import { getDictionary } from "@/i18n/get-dictionary";
 import { Locale } from "@/i18n/i18n-config";
 
-export default async function ApplicationPage({ params: { lang } }: { params: { lang: Locale } }) {
+export default async function ApplicationPage({ params }: { params: { lang: Locale } }) {
+  const { lang } = params;
   const dictionary = await getDictionary(lang);
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
